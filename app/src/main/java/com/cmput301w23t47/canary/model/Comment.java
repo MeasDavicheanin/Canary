b4fcf74af85802fc7ded4d0b8d741edd2be1b0ee
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-02-17 14:39:26 -0700

    Add models for problem

---
+++ app/src/main/java/com/cmput301w23t47/canary/model/Comment.java
@@ -0,0 +1,13 @@
+package com.cmput301w23t47.canary.model;
+
+/**
+ * Class for comment
+ */
+public class Comment {
+    // Player who added the comment
+    private Player player;
+    // The Qr code that is commented
+    private QRCode qrCode;
+    // The message in the comment
+    private String message;
+}
