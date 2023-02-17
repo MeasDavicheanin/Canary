93ad5297bfddb2860243eb365e35a1850d20fac0
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-02-17 14:46:30 -0700

    Add score calculator

---
+++ app/src/main/java/com/cmput301w23t47/canary/controller/ScoreCalculator.java
@@ -0,0 +1,17 @@
+package com.cmput301w23t47.canary.controller;
+
+import com.cmput301w23t47.canary.model.QRCode;
+
+/**
+ * Calculates the score for the QR
+ */
+public class ScoreCalculator {
+    /**
+     * Calculates the score for the given QR
+     * @param qrCode (QRCode): The qr code to find the score of
+     * @return the calculated score
+     */
+    static long calculateScore(QRCode qrCode) {
+        return 0L;
+    }
+}
