47f2b61948e3510d22ce8e36f1bedb188f46e3fe
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-02-18 12:58:43 -0700

    Add empty activity for scan qr code

---
+++ app/src/main/java/com/cmput301w23t47/canary/view/activity/ScanQRCodeActivity.java
@@ -0,0 +1,16 @@
+package com.cmput301w23t47.canary.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmput301w23t47.canary.R;
+
+public class ScanQRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qrcode);
+    }
+}
