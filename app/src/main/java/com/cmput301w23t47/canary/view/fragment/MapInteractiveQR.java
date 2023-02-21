b6467904d5ca4f2d5286425f28ac053c230e90d9
Ajharris1344 <andrewharris1344@gmail.com>
2023-02-21 02:47:09 -0700

    Map Basics
    Super barebones I want to talk to everybody else and get their input on what to add before I do anything.
    But it seems like this task will be suprisingly easy

---
+++ app/src/main/java/com/cmput301w23t47/canary/view/fragment/MapInteractiveQR.java
@@ -0,0 +1,21 @@
+package com.cmput301w23t47.canary.view.fragment;
+
+import com.cmput301w23t47.canary.model.QRCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapInteractiveQR {
+
+    private List<QRCode> QRList = new ArrayList<>();
+
+    public MapInteractiveQR() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+
+
+
+
+}
