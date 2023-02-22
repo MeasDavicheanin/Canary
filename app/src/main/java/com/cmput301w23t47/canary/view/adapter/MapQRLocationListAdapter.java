313426ce898c543cf3d7a6d249db993f38cbe0e0
Ajharris1344 <andrewharris1344@gmail.com>
2023-02-22 12:25:50 -0700

    Crashed just going to commit these changes for now

--- app/src/main/java/com/cmput301w23t47/canary/view/adapter/MapQRLocationListAdapter.java
+++ app/src/main/java/com/cmput301w23t47/canary/view/adapter/MapQRLocationListAdapter.java
@@ -1,10 +1,12 @@
-package com.cmput301w23t47.canary.model;
+package com.cmput301w23t47.canary.view.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.cmput301w23t47.canary.model.QRCode;
 
 import java.util.List;
 
