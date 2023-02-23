895fca387c368e8c3d1c6ba25e5aead600a53e93
unknown <jsjugal81@gmail.com>
2023-02-23 16:43:56 -0700

    Updated Fragment with data

--- app/src/main/java/com/cmput301w23t47/canary/view/fragment/QRCodeView.java
+++ app/src/main/java/com/cmput301w23t47/canary/view/fragment/QRCodeView.java
@@ -4,58 +4,59 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.cmput301w23t47.canary.R;
+import com.cmput301w23t47.canary.model.Location;
+import com.cmput301w23t47.canary.model.PlayerQRCode;
+import com.cmput301w23t47.canary.model.Snapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link QRCodeView#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
+
 public class QRCodeView extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private PlayerQRCode currentPlayerQRCode;
+    private TextView currentQRCodeName;
+    private TextView currentQRCodeScore;
+    private TextView currentQRCodeScanDateTime;
+    private TextView currentQRCodeScanLocation;
+    private ImageView currentQRCodeImage;
     ListView commentsListView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public QRCodeView() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QRCodeView.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QRCodeView newInstance(String param1, String param2) {
-        QRCodeView fragment = new QRCodeView();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public QRCodeView(PlayerQRCode currentPlayerQRCode) {
+        this.currentPlayerQRCode = currentPlayerQRCode;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+
+    public void updateFragmentData(){
+        String name = currentPlayerQRCode.getName();
+        Location location = currentPlayerQRCode.getLocation();
+        Snapshot qrCodeImage = currentPlayerQRCode.getSnapshot();
+        long score = currentPlayerQRCode.getQrCode().getScore();
+        // **Need to set currentQRCodeScanLocation, currentQRCodeImage, currentQRCodeScanDateTime**
+        // we don't have any scan date time for the player qr codes yet
+        if(currentPlayerQRCode != null){
+            if(name != null)
+                currentQRCodeName.setText(name);
+            if(score >= 0)
+                currentQRCodeScore.setText("Score "+ String.valueOf(score)+ " Pts");
+            if(qrCodeImage != null){
+                // fill this once snapshot class is complete
+            }
+            if(location != null){
+                //fill this once location class is complete
+            }
         }
     }
 
@@ -63,6 +64,14 @@ public class QRCodeView extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr_code_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_qr_code_view, container, false);
+        currentPlayerQRCode = getArguments().getParcelable("playerQRCode");
+        currentQRCodeName = view.findViewById(R.id.QRCodeName);
+        currentQRCodeScore = view.findViewById(R.id.QRCodeScore);
+        currentQRCodeScanLocation = view.findViewById(R.id.QRCardLocation);
+        currentQRCodeScanDateTime = view.findViewById(R.id.QRCodeScanDateTime);
+        currentQRCodeImage = view.findViewById(R.id.QRCodeImage);
+        updateFragmentData();
+        return view;
     }
 }
