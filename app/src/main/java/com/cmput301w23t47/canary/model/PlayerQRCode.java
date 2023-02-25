90dcefbe4b374f3fa6902413ab05adf272fc1f6e
Davin <71451687+bc-davin@users.noreply.github.com>
2023-02-24 20:03:01 -0700

    Work on the Gen Words
    Please review and provides feedback

--- app/src/main/java/com/cmput301w23t47/canary/model/PlayerQRCode.java
+++ app/src/main/java/com/cmput301w23t47/canary/model/PlayerQRCode.java
@@ -16,68 +16,10 @@ public class PlayerQRCode {
     // Location of the QR
     private Location location;
     // name of the QR
-    private String name;
+    GenWords genWords = new GenWords();
+    private final String name=genWords.getWord();
     // Snapshot of the QR
     private Snapshot snapshot;
-    Faker faker = new Faker();
-    /**
-     * Constructor for the randomGenName
-     * @param: name
-     * @return two combine gen name
-     **/
-    public String randomGenName(String name) {
-        Random random = new Random();
-        String genName="";
-        for (int i=0;i<2;i++){
-            genName+=produceName(random.nextInt(10));
-        }
-        return genName;
-    }
-    /**
-        * Constructor for the produceName
-        * @param: choice
-        * @return returnName
-        **
-    **/
-    public String produceName(int choice){
-        String returnName;
-        switch (choice){
-            case 0:
-                returnName = faker.cat().name();
-                break;
-            case 1:
-                returnName = faker.dog().name();
-                break;
-            case 2:
-                returnName = faker.company().name();
-                break;
-            case 3:
-                returnName=faker.color().name();
-                break;
-            case 4:
-                returnName=faker.country().name();
-                break;
-            case 5:
-                returnName=faker.currency().name();
-                break;
-            case 6:
-                returnName=faker.gameOfThrones().city();
-                break;
-            case 7:
-                returnName=faker.university().name();
-                break;
-            case 8:
-                returnName=faker.country().capital();
-                break;
-            case 9:
-                returnName=faker.artist().name();
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + choice);
-        }
-        return returnName;
-    }
-
 
 
 }
---
+++ app/src/test/java/com/cmput301w23t47/canary/GenWordsTest.java
@@ -0,0 +1,13 @@
+package com.cmput301w23t47.canary;
+import com.cmput301w23t47.canary.model.GenWords;
+
+import org.junit.Test;
+public class GenWordsTest {
+    @Test
+    public void testGenWords() {
+       GenWords genWords= new GenWords();
+       String word = genWords.getWord();
+       assert(word.length() > 0);
+    }
+    // @Test for uniquness of words
+}
