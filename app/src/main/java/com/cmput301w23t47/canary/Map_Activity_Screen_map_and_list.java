616c7f2f6ed2159bc79ca6affe5bb8a2d63552d3
Ajharris1344 <andrewharris1344@gmail.com>
2023-03-05 04:37:32 -0700

    added auto complete
    autocomplete does exactly what it says
    should be able to edit so that it only works certain distance and on qr codes

--- app/src/main/java/com/cmput301w23t47/canary/Map_Activity_Screen_map_and_list.java
+++ app/src/main/java/com/cmput301w23t47/canary/Map_Activity_Screen_map_and_list.java
@@ -4,59 +4,224 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.type.LatLng;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Map_Activity_Screen_map_and_list extends AppCompatActivity {
 
     private static final String TAG = "Map_Activity_Screen_map_and_list";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int DEFAULT_ZOOM = 15;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    //widgets
+    private AutoCompleteTextView mSearchText;
+    private ImageView mGps;
+
+
+
     //vars
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    /*
+    THIS NEEDS TO UPDATE IN REALTIME
+    so the list
+
+    SO there is a difference between import that takes things in the form of
+    public static void distanceBetween (double startLatitude,
+                double startLongitude,
+                double endLatitude,
+                double endLongitude,
+                float[] results)
+
+    This will give the distance between two points in meters
+    so just have something like
+    Textview distance = findbyview
+    get the text
+    turn it into an integer
+    make sure it is positive ( could be done by the if)
+    for( i=0; i<markers.size(); i++){
+        if(distanceBetween(markers.get(i), markers.get(i+1)) < distance){
+            //do something
+            // add to list
+            // and/or place on map
+        }
+     */
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_screen_map_and_list);
+        mSearchText = (AutoCompleteTextView) findViewById(R.id.input_search);
+        mGps = (ImageView) findViewById(R.id.ic_gps);
+
         getLocationPermission();
+
     }
+
+    private void init(){
+        Log.d(TAG, "init: initializing");
+
+        mSearchText.setOnItemClickListener(new TextView.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getAction() == KeyEvent.ACTION_UP){
+                    //execute our method for searching
+                    geoLocate();
+                }
+                return false;
+            }
+        });
+
+        mGps.setOnClickListener(v -> {
+            Log.d(TAG, "onClick: clicked gps icon");
+            getDeviceLocation();
+        });
+
+        hideSoftKeyboard();
+
+    }
+
+    // this entire thing isn't explicity wrong but it is for finding things that arn't relevent to our program
+    // Think we have to swap the geocoder with the list of our value
+    // this doesn't display on the map instead what it does is it takes the location from the search bar and marks it on the map
+    private void geoLocate(){
+        Log.d(TAG, "geoLocate: geolocating");
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(Map_Activity_Screen_map_and_list.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+            Log.e(TAG, "geoLocate: IOException: " + e.getMessage() );
+        }
+
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+            Log.d(TAG, "geoLocate: found a location: " + address.toString());
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM,
+                    address.getAddressLine(0) , address.getAddressLine(0));
+        }
+    }
+
+}
+
 
     private void getDeviceLocation(){
-        log.d(TAG, "getDeviceLocation: getting the devices current location");
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mLocationPermissionsGranted){
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM, "Your Location");
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(Map_Activity_Screen_map_and_list.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage() );
+        }
 
 
     }
 
-
+    private void moveCamera(LatLng latLng, float zoom, String title){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        // this adds a marker to the map
+        if(!title.equals("Your Location")){
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(title);
+            mMap.addMarker(options);
+        }
+        hideSoftKeyboard();
+    }
 
 
     //ALL OF THIS WAS VIDEO 4
     private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
-        mapFragment.getMapAsync(Map_Activity_Screen_map_and_list.this);
-    }
-    @Override
-    public void onMapReady(GoogleMap googleMap){
-        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
-        mMap = googleMap;
+        mapFragment.getMapAsync( new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Toast.makeText(Map_Activity_Screen_map_and_list.this, "Map is Ready", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onMapReady: map is ready");
+                mMap = googleMap;
+
+                if(mLocationPermissionsGranted){
+                    getDeviceLocation();
+
+                    if(ActivityCompat.checkSelfPermission(Map_Activity_Screen_map_and_list.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Map_Activity_Screen_map_and_list.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                        return;
+                    }
+                    mMap.setMyLocationEnabled(true);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                }
+            }
+        });
     }
 
 
     private void getLocationPermission(){
-        string[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION};
 
         if(contextCompat.checkSelfPermission(this.getApplicationContext(),
@@ -81,6 +246,7 @@ public class Map_Activity_Screen_map_and_list extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        Log.d(TAG, "onRequestPermissionResult: called.");
         mLocationPermissionsGranted = false;
 
         switch(requestCode){
@@ -89,11 +255,11 @@ public class Map_Activity_Screen_map_and_list extends AppCompatActivity {
                     for(int i = 0; i < grantResults.length; i++){
                         if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
                             mLocationPermissionsGranted = false;
-                            log.d(TAG, "onRequestPermissionResult: permission failed");
+                            Log.d(TAG, "onRequestPermissionResult: permission failed");
                             return;
                         }
                     }
-                    log.d(TAG, "onRequestPermissionResult: permission granted");
+                    Log.d(TAG, "onRequestPermissionResult: permission granted");
                     mLocationPermissionsGranted = true;
                     //initialize our map
                     initMap();
@@ -106,22 +272,26 @@ public class Map_Activity_Screen_map_and_list extends AppCompatActivity {
 
     //Since this is the activity that will hold the map it is location where we will check the map
     public boolean isServicesOK(){
-        log.d(TAG, "isServicesOK: checking google services version");
+        Log.d(TAG, "isServicesOK: checking google services version");
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(Map_Activity_Screen_map_and_list.this);
         if(available == ConnectionResult.SUCCESS){
             //everything is fine and the user can make map requests
-            log.d(TAG, "isServicesOK: Google Play Services is working");
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
             return true;
         }
         else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
             //an error occured but we can resolve it
-            log.d(TAG, "isServicesOK: an error occured but we can fix it");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(Map_Activity_Screen_map_and_list.this, available, ERROR_DIALOG_REQUEST);
-            dialog.show();
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog diaLog = GoogleApiAvailability.getInstance().getErrorDiaLog(Map_Activity_Screen_map_and_list.this, available, ERROR_DIALOG_REQUEST);
+            diaLog.show();
         }
         else{
             Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
         }
         return false;
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
