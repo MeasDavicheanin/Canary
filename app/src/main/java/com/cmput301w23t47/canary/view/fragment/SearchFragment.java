fae17cd8d5c25f116d73389499f681806a717b30
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-03-10 12:15:34 -0700

    Fix failure

--- app/src/main/java/com/cmput301w23t47/canary/view/fragment/SearchFragment.java
+++ app/src/main/java/com/cmput301w23t47/canary/view/fragment/SearchFragment.java
@@ -1,24 +1,11 @@
 package com.cmput301w23t47.canary.view.fragment;
 
 
-import android.app.appsearch.SearchResult;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.cmput301w23t47.canary.R;
 import com.cmput301w23t47.canary.model.Player;
 import com.cmput301w23t47.canary.view.adapter.PlayerSeachAdapter;
-import com.google.android.material.search.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
