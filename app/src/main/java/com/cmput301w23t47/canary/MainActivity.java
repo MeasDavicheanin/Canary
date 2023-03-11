f1c4f8c74ec752584a01b2aa9fcbfbfa537cc631
Ajharris1344 <andrewharris1344@gmail.com>
2023-03-10 20:01:25 -0700

    Update MainActivity.java

--- app/src/main/java/com/cmput301w23t47/canary/MainActivity.java
+++ app/src/main/java/com/cmput301w23t47/canary/MainActivity.java
@@ -24,6 +24,7 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 import android.Manifest;
 
